0e61ea36af1a5157a200a02885daeeabbdc7bc5d
looedaking <luisramirez2@cox.net>
2018-03-08 00:00:14 -0600

    added css folder for tos' styles and abstracted javascript to its on file

---
+++ assets/javascript/index.js
@@ -0,0 +1,44 @@
+$(function(){
+			var socket = io.connect();	
+			var $messageForm = $('#messageForm');
+			var $message = $('#message');
+			var $chat = $('#chat');
+			var $messageArea = $('#messageArea');
+			var $userFormArea = $('#userFormArea');
+			var $userForm = $('#userForm');
+			var $users = $('#users');
+			var $username = $('#username');
+			var $password = $('#password');
+
+			$messageForm.submit(function(e){
+				e.preventDefault();
+				socket.emit('send message', $message.val());
+				$message.val('');
+			});
+
+			socket.on('new message', function(data){
+				$chat.append('<div class="well"><strong>'+ data.user +'</strong>: ' + data.msg +'</div>');	
+			});
+
+			$userForm.submit(function(e){
+				e.preventDefault();
+					socket.emit('new user', {username:$username.val(), password:$password.val()}, function(data){
+					console.log(data);
+						if(data){
+							$userFormArea.hide();
+							$messageArea.show();
+						}
+					});
+				
+				$username.val('');
+				$password.val('');
+			});
+
+			socket.on('get users', function(data){
+				var html = '';
+				for(i=0; i<data.length; i++){
+					html += '<li class="list-group-item">' + data[i] + ' </li>'
+				}
+				$users.html(html);
+			})
+		});
